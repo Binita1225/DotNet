463c246144901ab61e4b0fbd88f635c2a59089b3
Binita1225 <160878726+Binita1225@users.noreply.github.com>
2024-06-05 17:55:29 +0545

    single inheritance lab work uploaded

---
+++ SingleInheritanceLab8/SingleInheritanceLab8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// Base class
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating.");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("Animal is sleeping.");
+    }
+}
+
+// Derived class inheriting from the base class (Single Inheritance)
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog is barking.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating an object of the derived class
+        Dog myDog = new Dog();
+
+        // Accessing methods from the base class
+        myDog.Eat();
+        myDog.Sleep();
+
+        // Accessing method from the derived class
+        myDog.Bark();
+    }
+}
